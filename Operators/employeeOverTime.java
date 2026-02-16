7153ab2d333b3966f12503372b72378753f79444
Gokulnath <gokulj172@gmail.com>
2026-02-16 14:42:03 +0530

    Add files via upload
    solved 03 problem

---
+++ Operators/employeeOverTime.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+public class employeeOverTime {
+    public static void main(String[] args) {
+        Scanner sc =new  Scanner (System.in);
+        int hours=sc.nextInt();
+        int attendance=sc.nextInt();
+
+        if(hours>40 && attendance>90){
+            System.out.println("Eligible");
+        }
+        else{
+            System.out.println("Not Eligible");
+
+        }
+    }
+}
